2f71a49db3d3e346b3121364ad75f0ba5f8c769b
bala418 <balavtwo@gmail.com>
2022-01-26 21:49:00 +0530

    Add heap process

--- encode.c
+++ encode.c
@@ -5,6 +5,15 @@ freq_map m[256], unique[256];
 int total_char_count;
 int unique_char_count;
 
+Node *m1[256];
+Node *unique1[256];
+
+Node *head;
+
+// ********************************************
+// Menu functions
+// ********************************************
+
 void encode() {
     encode_menu();
 }
@@ -135,49 +144,13 @@ void encode_confirm(char *file_name) {
     }
 }
 
-void encode_begin(char *file_name) {
-    printf("\n\n=====>\tEncoding File\t<=====\n");
-    printf("\nFile name : %s\n", file_name);
+// ********************************************
+// Helper functions
+// ********************************************
 
-    encode_frequency(file_name);
-
-    encode_done();
-}
-
-void encode_frequency(char *file_name) {
-
-    FILE *fp;
-    fp = fopen(file_name, "r");
-
-    total_char_count = 0;
-    unique_char_count = 0;
-
-    char ch;
-    int pos;
-    while ((ch = fgetc(fp)) != EOF) {
-        pos = ch;
-        m[pos].character = ch;
-        m[pos].frequency++;
-        total_char_count++;
-    }
-    fclose(fp);
-
-    // Copying the map to the final shorter map
-    for (int i = 0; i < 256; i++) {
-        if (m[i].frequency > 0) {
-            unique[unique_char_count] = m[i];
-            unique_char_count++;
-        }
-    }
-    printf("\nThe final hashmap of characters and their frequencies are : \n");
-    for (int i = 0; i < unique_char_count; i++) {
-        printf("\n%c - %d", unique[i].character, unique[i].frequency);
-    }
-}
-
+// Print binary tree in tree form
 int rec[1000006];
-
-void printTree(struct Node *curr, int depth) {
+void print_tree(struct Node *curr, int depth) {
     int i;
     if (curr == NULL)
         return;
@@ -187,13 +160,164 @@ void printTree(struct Node *curr, int depth) {
             printf("%s---", rec[depth - 1] ? "l" : "l");
         else
             printf("%s   ", rec[i] ? "l" : "  ");
-    printf("%d\n", curr->symbol);
+    printf("%d\n", curr->freq);
     rec[depth] = 1;
-    printTree(curr->left, depth + 1);
+    print_tree(curr->left, depth + 1);
     rec[depth] = 0;
-    printTree(curr->right, depth + 1);
+    print_tree(curr->right, depth + 1);
 }
 
+// create a node
+Node *create_node(int f, char c) {
+    Node *temp = (Node *)malloc(sizeof(Node));
+    temp->freq = f;
+    temp->character = c;
+    temp->right = NULL;
+    temp->left = NULL;
+    return temp;
+}
+
+// Node *insert(Node *root, int data) {
+//     if (root == NULL) {
+
+//         //create the new node and return it to the parent
+//         return createNode(data);
+//     } else if (root->data > data) {
+//         //new root.left will be after inserting it
+//         root->left = insert(root->left, data);
+//     } else if (root->data < data) {
+//         //new root.right will be after inserting it
+//         root->right = insert(root->right, data);
+//     }
+//     //finally return the root
+//     return root;
+// }
+
+// ********************************************
+// Encoding functions
+// ********************************************
+
+// All function calls for encoding
+void encode_begin(char *file_name) {
+    printf("\n\n=====>\tEncoding File\t<=====\n");
+    printf("\nFile name : %s\n", file_name);
+
+    encode_frequency(file_name);
+
+    build_heap();
+
+    encode_done();
+}
+
+// Calculate frequency of all characters
+void encode_frequency(char *file_name) {
+
+    FILE *fp;
+    fp = fopen(file_name, "r");
+
+    total_char_count = 0;
+    unique_char_count = 0;
+
+    //initializing it to 0
+    for (int i = 0; i < 256; i++) {
+        m[i].frequency = 0;
+        // m1[i]->freq = 0;
+    }
+
+    char ch;
+    int pos;
+    while ((ch = fgetc(fp)) != EOF) {
+        pos = ch;
+        m[pos].character = ch;
+        // m1[pos]->character = ch;
+        m[pos].frequency++;
+        // m1[pos]->freq++;
+        total_char_count++;
+    }
+    fclose(fp);
+
+    // Copying the map to the final shorter map
+    for (int i = 0; i < 256; i++) {
+        if (m[i].frequency > 0) {
+            unique[unique_char_count] = m[i];
+            // unique1[unique_char_count] = m1[i];
+            unique_char_count++;
+        }
+    }
+    printf("\nThe final hashmap of characters and their frequencies are : \n");
+    for (int i = 0; i < unique_char_count; i++) {
+        unique1[i] = create_node(unique[i].frequency, unique[i].character);
+        printf("\n%c - %d", unique[i].character, unique[i].frequency);
+    }
+    printf("\n");
+}
+
+// function to build the heap
+MinHeap *build_heap() {
+
+    // for (int i = 0; i < unique_char_count; i++) {
+    //     printf("\n%c - %d", unique1[i]->character, unique1[i]->freq);
+    //     // printf("\n%c - %d", unique[i].character, unique[i].frequency);
+    // }
+
+    // printf("\n%d ", minHeap->capacity);
+    // printf("\n%d ", minHeap->size);
+    // for (int i = 0; i < minHeap->size; ++i)
+    //     printf("%d\n", minHeap->array[i]->freq);
+    struct MinHeap *minHeap = createMinH(unique_char_count);
+
+    for (int i = 0; i < unique_char_count; ++i)
+        minHeap->array[i] = unique1[i];
+
+    minHeap->size = unique_char_count;
+    buildMinHeap(minHeap);
+
+    return minHeap;
+}
+struct MinHeap *createMinH(unsigned capacity) {
+    struct MinHeap *minHeap = (struct MinHeap *)malloc(sizeof(struct MinHeap));
+
+    minHeap->size = 0;
+
+    minHeap->capacity = capacity;
+
+    minHeap->array = (struct Node **)malloc(minHeap->capacity * sizeof(struct Node *));
+    return minHeap;
+}
+
+void swapMinHNode(struct Node **a, struct Node **b) {
+    struct Node *t = *a;
+    *a = *b;
+    *b = t;
+}
+
+// Heapify
+void minHeapify(struct MinHeap *minHeap, int idx) {
+    int smallest = idx;
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
+
+    if (left < minHeap->size && minHeap->array[left]->freq < minHeap->array[smallest]->freq)
+        smallest = left;
+
+    if (right < minHeap->size && minHeap->array[right]->freq < minHeap->array[smallest]->freq)
+        smallest = right;
+
+    if (smallest != idx) {
+        swapMinHNode(&minHeap->array[smallest], &minHeap->array[idx]);
+        minHeapify(minHeap, smallest);
+    }
+}
+
+void buildMinHeap(struct MinHeap *minHeap) {
+    int n = minHeap->size - 1;
+    int i;
+
+    for (i = (n - 1) / 2; i >= 0; --i)
+        minHeapify(minHeap, i);
+}
+
+// indicates finishing of the process
 void encode_done() {
     printf("\nEncoding completed successfully....\n");
 }
--- encode.h
+++ encode.h
@@ -5,9 +5,10 @@ struct Node {
     struct Node *left;  // used in encode tree ; '0'
     struct Node *right; // used in encode tree ; '1'
     int freq;           // frequency of symbol in file
-    char symbol;        // the symbol assigned to this node (char)
+    char character;     // the symbol assigned to this node (char)
     char code;
 };
+
 typedef struct Node Node;
 
 struct freq_map {
@@ -15,6 +16,13 @@ struct freq_map {
     int frequency;
 };
 
+struct MinHeap {
+    unsigned size;
+    unsigned capacity;
+    struct Node **array;
+};
+
+typedef struct MinHeap MinHeap;
 typedef struct freq_map freq_map;
 
 // function to access all the encode functions
@@ -32,15 +40,25 @@ void encode_file();
 // function to confirm the encoding process
 void encode_confirm();
 
+// function to print binary tree
+void print_tree();
+
 // Start the encoding process
 void encode_begin();
 
 // count the frequency
 void encode_frequency();
 
+// function to build the heap
+MinHeap *build_heap();
+MinHeap *createMinH();
+void swapMinHNode();
+void minHeapify();
+void buildMinHeap();
 // indicates finishing of the process
 void encode_done();
 
-void printTree();
-
 #endif
+
+// sample inputs
+// ABBCDBCCDAABBEEEBEAB
